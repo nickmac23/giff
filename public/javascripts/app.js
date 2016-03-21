3dfff950cc2137251b31e63f8a28a2ddc2472123
nickmac <nickpmacintyre@gmail.com>
2016-03-20 20:55:09 -0600

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", function() {
+	// Grab elements, create settings, etc.
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		video = document.getElementById("video"),
+		videoObj = { "video": true },
+		errBack = function(error) {
+			console.log("Video capture error: ", error.code);
+		};
+
+	// Put video listeners into place
+
+
+		navigator.webkitGetUserMedia(videoObj, function(stream){
+			video.src = window.URL.createObjectURL(stream);
+			video.play();
+		}, errBack);
+
+
+
+
+  document.getElementById("snap").addEventListener("click", function() {
+	context.drawImage(video, 0, 0, canvas.width, canvas.height);
+});
+}, false);
