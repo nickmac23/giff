0bfc4d0c521f25b211acda881d9505c9a8122ffa
nickmac <nickpmacintyre@gmail.com>
2016-03-21 13:07:12 -0600

    working on saving images

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,12 @@
 window.addEventListener("DOMContentLoaded", function() {
 	// Grab elements, create settings, etc.
 	var canvas = document.getElementById("canvas"),
+			button = document.getElementById('button');
 		context = canvas.getContext("2d"),
 		video = document.getElementById("video"),
 		videoObj = { "video": true },
+		videoSrc = [],
+		count = 1;
 		errBack = function(error) {
 			console.log("Video capture error: ", error.code);
 		};
@@ -14,13 +17,23 @@ window.addEventListener("DOMContentLoaded", function() {
   document.getElementById("snap").addEventListener("click", function() {
 		navigator.webkitGetUserMedia(videoObj, function(stream){
 			video.src = window.URL.createObjectURL(stream);
-			// video.play();
+
 		}, errBack);
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+
+		context.drawImage(video, 0, 0, canvas.width, canvas.height);
+		///saves image to array
+		videoSrc.push(canvas.toDataURL('image/png'));
+
+
 
 
 
 
   });
-
+	button.addEventListener('click', function(){
+		count++
+		//prints image from array
+		photo.setAttribute('src', videoSrc[count]);
+	})
 }, false);
