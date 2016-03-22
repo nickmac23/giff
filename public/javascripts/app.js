d2545bb16452c1008e473a92f3243f92914c5e76
nickmac <nickpmacintyre@gmail.com>
2016-03-22 10:22:17 -0600

    styling

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,13 @@
+
 window.addEventListener("DOMContentLoaded", function() {
 	// Grab elements, create settings, etc.
 	var canvas = document.getElementById("canvas"),
 			button = document.getElementById('button');
-		context = canvas.getContext("2d"),
-		video = document.getElementById("video"),
-		videoObj = { "video": true },
-		videoSrc = [],
-		count = 1;
+			context = canvas.getContext("2d"),
+			video = document.getElementById("video"),
+			videoObj = { "video": true },
+			videoSrc = [],
+			count = 0;
 		errBack = function(error) {
 			console.log("Video capture error: ", error.code);
 		};
@@ -14,26 +15,30 @@ window.addEventListener("DOMContentLoaded", function() {
 	// Put video listeners into place
 
 
-  document.getElementById("snap").addEventListener("click", function() {
 		navigator.webkitGetUserMedia(videoObj, function(stream){
 			video.src = window.URL.createObjectURL(stream);
-
+			video.play();
 		}, errBack);
 
 
+	document.getElementById("snap").addEventListener("click", function() {
+		var val = text.value;
 		context.drawImage(video, 0, 0, canvas.width, canvas.height);
+		context.font = "20px serif";
+		context.fillText(val, 20, 200);
+		videoSrc.push(canvas.toDataURL('image/pgn'));
+
 		///saves image to array
-		videoSrc.push(canvas.toDataURL('image/png'));
-
-
-
-
-
-
   });
+	var text = document.getElementById("text1")
+
 	button.addEventListener('click', function(){
-		count++
 		//prints image from array
 		photo.setAttribute('src', videoSrc[count]);
+		console.log(photo.src);
+		count++
+		if (count === videoSrc.length) {
+			count = 0;
+		}
 	})
 }, false);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var arr = fs.readFile('txt').join('')
+  res.json(arr);
+  // res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
